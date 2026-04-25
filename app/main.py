f30c9789881a22bc952f1bce8edcffa4674e5fe6
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2026-04-26 00:54:41 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ PORT = 6379
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
 
